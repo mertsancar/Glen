f842d21bb5bf15dfa6980aea482e76683458ee54
Mert <mertsancar12@gmail.com>
2023-03-09 17:53:01 +0300

    Create Enemy_Follow/Patrol anim

--- Assets/Scripts/EnemyFollowBehaviour.cs
+++ Assets/Scripts/EnemyFollowBehaviour.cs
@@ -6,17 +6,19 @@ public class EnemyFollowBehaviour : StateMachineBehaviour
 {
     private Transform _playerPos;
     private static readonly int IsFollowing = Animator.StringToHash("isFollowing");
+    private static readonly int IsPatrolling = Animator.StringToHash("isPatrolling");
+
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         _playerPos = GameObject.FindGameObjectWithTag("Player").transform;
     }
-
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         if (Vector2.Distance(_playerPos.position, animator.transform.position) > 5f)
         {
             animator.SetBool(IsFollowing, false);
+            animator.SetBool(IsPatrolling, true);
         }
         else
         {
@@ -24,21 +26,8 @@ public class EnemyFollowBehaviour : StateMachineBehaviour
         }
     }
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        animator.SetBool(IsPatrolling, true);
     }
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
